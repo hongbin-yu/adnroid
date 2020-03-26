2efde56ab6d8ef80d5b3be60b10a464c43e3165a
hongbin-yu <hongbin.yu413@gmail.com>
2020-03-26 13:54:11 -0400

    add lisentner

--- app/src/main/java/wang/switchy/hin2n/storage/db/base/DaoMaster.java
+++ app/src/main/java/wang/switchy/hin2n/storage/db/base/DaoMaster.java
@@ -22,11 +22,13 @@ public class DaoMaster extends AbstractDaoMaster {
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
         N2NSettingModelDao.createTable(db, ifNotExists);
+        ZerotierSettingModelDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
         N2NSettingModelDao.dropTable(db, ifExists);
+        ZerotierSettingModelDao.dropTable(db, ifExists);
     }
 
     /**
@@ -46,6 +48,7 @@ public class DaoMaster extends AbstractDaoMaster {
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
         registerDaoClass(N2NSettingModelDao.class);
+        registerDaoClass(ZerotierSettingModelDao.class);
     }
 
     public DaoSession newSession() {
